accf6518dbfc2486fddedaac554631744f1a7ba7
Ahuang0107 <Seto1920748235@126.com>
2021-05-19 04:16:05 +0800

    delete dependencies

--- build.gradle.kts
+++ build.gradle.kts
@@ -26,7 +26,7 @@ dependencies {
 	implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
 	implementation("org.jetbrains.kotlin:kotlin-reflect")
 	implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
-	implementation("com.github.ifrankwang:project-utils:2.1.2")
+
 	testImplementation("org.springframework.boot:spring-boot-starter-test")
 }
 
